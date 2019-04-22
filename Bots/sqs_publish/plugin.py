e7dde0ce7e13b4b569aa927dc178ef8186d43536
Scott Longhofer <scott@MacBook-Pro-3.local>
2019-04-22 09:31:10 -0400

    Added SQS Publish Bot Action

---
+++ Bots/sqs_publish/plugin.py
@@ -0,0 +1,25 @@
+"""
+Main plugin module for custom actions
+"""
+from DivvyPlugins.plugin_metadata import PluginMetadata
+
+
+class metadata(PluginMetadata):
+    """
+    Information about this plugin
+    """
+    version = '1.1'
+    last_updated_date = '2019-01-08'
+    author = 'Cloud49'
+    nickname = 'AWS SQS Publish'
+    default_language_description = 'Allows publishing a message to an SQS queue as an action.'
+    category = 'Actions'
+    managed = False
+
+
+def load():
+    pass
+
+
+def unload():
+    pass
